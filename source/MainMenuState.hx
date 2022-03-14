860013774c126ded393d80e983ca6ecc20b031ec
Umcaraxd <101530288+Umcaraxd@users.noreply.github.com>
2022-03-14 18:06:56 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -182,6 +182,10 @@ class MainMenuState extends MusicBeatState
 
 		changeItem();
 
+                #if android
+                addVirtualPad(UP_DOWN, A_B);
+                #end
+
 		super.create();
 	}
 
