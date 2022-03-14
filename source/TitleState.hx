a25fb964639a3dc1e91ea7e01e4a557c1ce09677
Umcaraxd <101530288+Umcaraxd@users.noreply.github.com>
2022-03-14 18:27:54 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -46,6 +46,9 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
 		
 		FlxG.fullscreen = FlxG.save.data.fullscreen;
 		#if polymod
